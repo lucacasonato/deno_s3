da4e761d174e42eace43891ee0986893f37f4d35
Luca Casonato <lucacasonato@yahoo.com>
2020-06-27 19:00:12 +0200

    Add getObject

--- mod.ts
+++ mod.ts
@@ -1,5 +1,9 @@
 import { AWSSignerV4, sha256 } from "./deps.ts";
-import { PutObjectResponse, PutObjectOptions } from "./types.ts";
+import {
+  GetObjectOptions,
+  PutObjectResponse,
+  PutObjectOptions,
+} from "./types.ts";
 
 export interface S3Config {
   bucket: string;
@@ -24,19 +28,33 @@ export class S3Client {
     path: string,
     method: string,
     headers: { [key: string]: string },
-    body: Uint8Array,
+    body?: Uint8Array | undefined,
   ): Promise<Response> {
     const url = `${this.#host}${path}`;
     const signedHeaders = this.#signer.sign("s3", url, method, headers, body);
-    const payloadHash = sha256(body, "utf8", "hex") as string;
+    if (body) {
+      signedHeaders["x-amz-content-sha256"] = sha256(
+        body,
+        "utf8",
+        "hex",
+      ) as string;
+    }
     return fetch(url, {
       method,
-      headers: {
-        ...signedHeaders,
-        "x-amz-content-sha256": payloadHash,
-      },
+      headers: signedHeaders,
       body,
     });
+  }
+
+  async getObject(
+    key: string,
+    options?: GetObjectOptions,
+  ): Promise<ReadableStream<Uint8Array>> {
+    const resp = await this._doRequest(key, "GET", {});
+    if (!resp.ok) {
+      throw new Error(`Failed to get object: ${resp.statusText}`);
+    }
+    return resp.body!;
   }
 
   async putObject(
@@ -50,7 +68,6 @@ export class S3Client {
     }
     const resp = await this._doRequest(key, "PUT", headers, body);
     if (!resp.ok) {
-      console.log(await resp.text());
       throw new Error(`Failed to put object: ${resp.statusText}`);
     }
     return {
--- types.ts
+++ types.ts
@@ -1,5 +1,14 @@
+// deno-lint-ignore no-empty-interface
+export interface GetObjectOptions {}
 export interface PutObjectOptions {
-  acl: "private" | "public-read" | "public-read-write" | "authenticated-read" | "aws-exec-read" | "bucket-owner-read" | "bucket-owner-full-control"
+  acl:
+    | "private"
+    | "public-read"
+    | "public-read-write"
+    | "authenticated-read"
+    | "aws-exec-read"
+    | "bucket-owner-read"
+    | "bucket-owner-full-control";
 }
 export interface PutObjectResponse {
   etag: string;
