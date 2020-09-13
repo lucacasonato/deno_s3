9dcd8c9b6fa52a43298419b4673f30613f854da4
Luca Casonato <lucacasonato@yahoo.com>
2020-09-13 20:46:57 +0200

    Update Deno and std

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { AWSSignerV4 } from "https://deno.land/x/aws_sign_v4@0.1.0/mod.ts";
-import { createHash } from "https://deno.land/std@0.68.0/hash/mod.ts";
+export { AWSSignerV4 } from "https://deno.land/x/aws_sign_v4@0.1.1/mod.ts";
+import { createHash } from "https://deno.land/std@0.69.0/hash/mod.ts";
 export function sha256Hex(data: string | Uint8Array): string {
   const hasher = createHash("sha256");
   hasher.update(data);
