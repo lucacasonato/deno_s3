36af6784e549b979281b9fbbc151ab9321db05f1
Elias Sjögreen <eliassjogreen1@gmail.com>
2021-01-18 19:35:59 +0100

    revert

--- deps.ts
+++ deps.ts
@@ -13,6 +13,6 @@ export function sha256Hex(data: string | Uint8Array): string {
   hasher.update(data);
   return hasher.toString("hex");
 }
-export { default as parseXML } from "https://denopkg.com/nekobato/deno-xml-parser/index.ts";
+export { default as parseXML } from "https://raw.githubusercontent.com/nekobato/deno-xml-parser/0bc4c2bd2f5fad36d274279978ca57eec57c680c/index.ts";
 export { decode as decodeXMLEntities } from "https://deno.land/x/html_entities@v1.0/lib/xml-entities.js";
 export { pooledMap } from "https://deno.land/std@0.83.0/async/pool.ts";
