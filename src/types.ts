573c19a2d8d5b83c198c41c4e2ce132d3076de27
Benjamin Fischer <c4spar@gmx.de>
2021-11-12 23:23:40 +0100

    move locationConstraint to CreateBucketOptions

--- src/types.ts
+++ src/types.ts
@@ -566,16 +566,7 @@ export type LocationConstraint =
   | "us-west-1"
   | "us-west-2";
 
-interface CreateBucketConfiguration {
-  /**
-   * Specifies the Region where the bucket will be created. If you don't
-   * specify a Region, the bucket is created in the US East (N. Virginia)
-   * Region (us-east-1).
-   */
-  locationConstraint?: LocationConstraint;
-}
-
-export interface CreateBucketOptions extends CreateBucketConfiguration {
+export interface CreateBucketOptions {
   /** The canned ACL to apply to the bucket */
   acl?:
     | "private"
@@ -603,4 +594,11 @@ export interface CreateBucketOptions extends CreateBucketConfiguration {
 
   /** Allows grantee to write the ACL for the applicable bucket. */
   grantWriteAcp?: string;
+
+  /**
+   * Specifies the Region where the bucket will be created. If you don't
+   * specify a Region, the bucket is created in the US East (N. Virginia)
+   * Region (us-east-1).
+   */
+  locationConstraint?: LocationConstraint;
 }
